edde9bbbc32d580d9a07411ec21146749a1d70fd
Vyra <suporte@vyratechnology.com>
2026-04-07 20:45:31 -0300

    Update year selection to show only 2025 and 2026 in document import

--- app/(dashboard)/imports/page.tsx
+++ app/(dashboard)/imports/page.tsx
@@ -193,9 +193,6 @@ export default function ImportsPage() {
                 <SelectContent>
                   <SelectItem value="2026">2026</SelectItem>
                   <SelectItem value="2025">2025</SelectItem>
-                  <SelectItem value="2024">2024</SelectItem>
-                  <SelectItem value="2023">2023</SelectItem>
-                  <SelectItem value="2022">2022</SelectItem>
                 </SelectContent>
               </Select>
             </div>
